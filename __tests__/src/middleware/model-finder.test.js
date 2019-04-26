5d14c070bc9176b1c5ffbd4c7e877800adc05513
Joseph <z@charmed.tech>
2019-04-25 18:16:45 -0700

    Add model-finder tests

--- __tests__/src/middleware/model-finder.test.js
+++ __tests__/src/middleware/model-finder.test.js
@@ -1,7 +1,27 @@
 'use strict';
 
+const cwd = process.cwd();
+const modelFinder = require(`${cwd}/src/middleware/model-finder.js`);
+
+const req = { params: { model: 'books' } };
+const res = {};
+const next = jest.fn();
+
+beforeAll(() => {
+  modelFinder(req, res, next);
+});
+
 describe('model-finder.js', () => {
-  it('should be good', () => {
-    expect(true).toBeTruthy();
+  it('should append an object to the request as a `model` property', () => {
+    expect(req.model).toBeDefined();
+  });
+  it('should call the `next` function', () => {
+    modelFinder(req, res, next);
+    expect(next).toHaveBeenCalled();
+  });
+  it('should call the `next` function if the model does not exist', () => {
+    const badReq = { params: { mode: 'dummy' } };
+    modelFinder(badReq, res, next);
+    expect(next).toHaveBeenCalled();
   });
 });
